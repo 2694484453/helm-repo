7a2b08ae264ea1ef446e149406c654b9de4502fd
‘gaopuguang_zz’ <gaopg09@chinatelecom.cn>
2024-12-09 10:48:53 +0800

    2024/12/09

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #安装prometheus
 helm install prometheus ./prometheus-24.3.0.tgz -n monitoring --create-namespace
 
+#
+helm install kubernetes-dashboard ./kubernetes-dashboard-7.5.0.tgz -n kubernetes-dashboard --create-namespace
+
+#
